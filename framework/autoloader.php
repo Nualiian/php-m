2d74743c8bf4fa22fb7d90ead1acd803566e113b
Richard Rosko <rosko.richard@gmail.com>
2016-12-21 07:40:41 +0100

    autoloader path fixed

--- framework/autoloader.php
+++ framework/autoloader.php
@@ -6,7 +6,7 @@
 spl_autoload_register(function($className) {
 	$namespace = str_replace("\\", "/", __NAMESPACE__);
 	$className = str_replace("\\", "/", $className);
-	$class = "framework/" . (empty($namespace) ? "" : $namespace . "/") . "{$className}.php";
+	$class = "framework" . (empty($namespace) ? "" : $namespace . "/") . "{$className}.php";
 
 	if (file_exists($class)) {
 		require_once($class);
